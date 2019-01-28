ec97f8f831bc82b7ff539663768ce022237fc587
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-01-28 20:18:31 +0200

    init

--- src/main/java/com/findMe/validator/AcceptValidator.java
+++ src/main/java/com/findMe/validator/AcceptValidator.java
@@ -1,16 +1,27 @@
 package com.findMe.validator;
 
+import com.findMe.exception.BadRequestException;
+import com.findMe.model.Relationship;
 import com.findMe.model.RelationshipStatus;
 
 public class AcceptValidator extends AbstractChainValidator {
+    private static final RelationshipStatus currentStatus = RelationshipStatus.REQUESTED;
+    private static final RelationshipStatus newStatus = RelationshipStatus.ACCEPTED;
+
 
     @Override
-    RelationshipStatus getCurrentStatus() {
-        return RelationshipStatus.REQUESTED;
-    }
+    Relationship validate(Relationship relationship, RelationshipStatus newStatus) throws BadRequestException {
+        if (relationship == null)
+            throw new BadRequestException("No requests from this user.");
 
-    @Override
-    RelationshipStatus getNewStatusStatus() {
-        return RelationshipStatus.ACCEPTED;
+        if (currentStatus.equals(newStatus)) {
+            relationship.setRelationshipStatus(newStatus);
+            return relationship;
+        }
+
+        if (super.getNextValidator() != null)
+            return super.getNextValidator() .validate(relationship, newStatus);
+        else
+            throw new BadRequestException("Action cannot be performed to this user.");
     }
 }
--- src/main/java/com/findMe/validator/DeleteValidator.java
+++ src/main/java/com/findMe/validator/DeleteValidator.java
@@ -1,17 +1,29 @@
 package com.findMe.validator;
 
 
+import com.findMe.exception.BadRequestException;
+import com.findMe.model.Relationship;
 import com.findMe.model.RelationshipStatus;
 
 public class DeleteValidator extends AbstractChainValidator{
+    private static final RelationshipStatus currentStatus = RelationshipStatus.ACCEPTED;
+    private static final RelationshipStatus newStatus = RelationshipStatus.DELETED;
+
 
     @Override
-    RelationshipStatus getCurrentStatus() {
-        return RelationshipStatus.ACCEPTED;
+    Relationship validate(Relationship relationship, RelationshipStatus newStatus) throws BadRequestException {
+        if (relationship == null)
+            throw new BadRequestException("No requests from this user.");
+
+        if (currentStatus.equals(newStatus)) {
+            relationship.setRelationshipStatus(newStatus);
+            return relationship;
+        }
+
+        if (super.getNextValidator()  != null)
+            return super.getNextValidator() .validate(relationship, newStatus);
+        else
+            throw new BadRequestException("Action cannot be performed to this user.");
     }
 
-    @Override
-    RelationshipStatus getNewStatusStatus() {
-        return RelationshipStatus.DELETED;
-    }
 }
