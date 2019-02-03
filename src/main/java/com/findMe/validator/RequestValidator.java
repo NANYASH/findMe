29ab875fb9b0267dc9eddc8bdcb2224af142c925
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-02-03 16:28:03 +0200

    init

--- src/main/java/com/findMe/validator/RequestValidator.java
+++ src/main/java/com/findMe/validator/RequestValidator.java
@@ -5,26 +5,27 @@ import com.findMe.exception.BadRequestException;
 import com.findMe.model.RelationshipStatus;
 
 public class RequestValidator extends AbstractChainValidator {
-    private static final RelationshipStatus CURRENT_STATUS = RelationshipStatus.DELETED;
+    private static final RelationshipStatus CURRENT_STATUS1 = RelationshipStatus.DELETED;
+    private static final RelationshipStatus CURRENT_STATUS2 = RelationshipStatus.CANCELED;
     private static final RelationshipStatus NEW_STATUS = RelationshipStatus.REQUESTED;
 
 
     @Override
     void validate() throws BadRequestException {
-        if (super.getRequestData().getRelationship()== null) {
-            if (super.getRequestData().getNewStatus().equals(NEW_STATUS) && super.getRequestData().getNumberOfOutgoingRequests() < 1)
+        if (super.getRequestData().getRelationship() == null) {
+            if (super.getRequestData().getNewStatus().equals(NEW_STATUS) && super.getRequestData().getNumberOfOutgoingRequests() < 10)
                 return;
             else
                 throw new BadRequestException("Max number of outgoing requests. Action cannot be performed.");
         }
 
-        if (CURRENT_STATUS.equals(super.getRequestData().getRelationship().getRelationshipStatus()) && NEW_STATUS.equals(super.getRequestData().getNewStatus())) {
-            if (super.getRequestData().getNumberOfOutgoingRequests() < 1)
+        if ((CURRENT_STATUS1.equals(super.getRequestData().getRelationship().getRelationshipStatus()) || CURRENT_STATUS2.equals(super.getRequestData().getRelationship().getRelationshipStatus()))
+                && NEW_STATUS.equals(super.getRequestData().getNewStatus())){
+            if (super.getRequestData().getNumberOfOutgoingRequests() < 10)
                 return;
             else
                 throw new BadRequestException("Max number of outgoing requests. Action cannot be performed.");
         }
-
         checkNextValidator(super.getNextValidator());
     }
 
