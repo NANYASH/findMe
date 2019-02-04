499282548a811b5661d737952d3c0435edf67001
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-02-04 22:42:48 +0200

    init

--- src/main/java/com/findMe/validator/RejectValidator.java
+++ src/main/java/com/findMe/validator/RejectValidator.java
@@ -12,7 +12,8 @@ public class RejectValidator extends AbstractChainValidator {
     @Override
     void validate() throws BadRequestException {
         if (CURRENT_STATUS.equals(super.getRequestData().getRelationship().getRelationshipStatus()) && NEW_STATUS.equals(super.getRequestData().getNewStatus()))
-            return;
+            if (super.getRequestData().getRelationship().getRelationshipId().getUserToId().equals(super.getRequestData().getUserToId()))
+                return;
 
         checkNextValidator(super.getNextValidator());
     }
