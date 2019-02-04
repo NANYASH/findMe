4430e153461a2ad0e79b4b41c231ac8a910f2302
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-02-04 20:52:38 +0200

    init

--- src/main/java/com/findMe/model/Post.java
+++ src/main/java/com/findMe/model/Post.java
@@ -7,7 +7,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "POST")
@@ -23,18 +25,31 @@ public class Post {
     @Column(name = "ID")
     private Long id;
 
-    @Column(name = "TEXT")
+
+    @Column(name = "TEXT", length = 200)
+    //200 symbols max
     private String text;
 
     @Column(name = "DATE_POSTED")
-    private Date datePosted;
+    private LocalDate datePosted;
+
+    //no validation
+    private String location;
 
     @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
-    @JoinColumn(name = "ID_USER_POSTED")
+    @JoinColumn(name = "USER_POSTED_ID")
     private User userPosted;
     //TODO
     // levels permissions
 
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "USER_PAGE_ID")
+    private User userPagePosted;
+
     //TODO
     //comments
+
+    @OneToMany(targetEntity = User.class, fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "USER_TAGGED_ID")
+    List<User> usersTagged;
 }
