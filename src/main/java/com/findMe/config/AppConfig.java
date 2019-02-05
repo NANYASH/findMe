1f6f718344dd903f5cd137ab6314d1c11d2d5af1
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-02-05 13:23:40 +0200

    init

--- src/main/java/com/findMe/config/AppConfig.java
+++ src/main/java/com/findMe/config/AppConfig.java
@@ -50,9 +50,9 @@ public class AppConfig implements WebMvcConfigurer{
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://findme.cnrx1jkycv8d.us-east-2.rds.amazonaws.com:5432/postgres");
-        dataSource.setUsername("main");
-        dataSource.setPassword("asol1998");
+        dataSource.setUrl("");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
         return dataSource;
     }
 
