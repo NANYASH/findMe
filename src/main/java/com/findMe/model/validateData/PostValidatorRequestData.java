a44b4b2dbdf5c9020c35eaefe1edae50d3a1b864
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-02-09 21:52:43 +0200

    init

--- src/main/java/com/findMe/model/validateData/PostValidatorRequestData.java
+++ src/main/java/com/findMe/model/validateData/PostValidatorRequestData.java
@@ -1,16 +1,11 @@
 package com.findMe.model.validateData;
 
 import com.findMe.model.Post;
-import com.findMe.model.Relationship;
-import com.findMe.model.User;
 import com.findMe.model.enums.RelationshipStatus;
-import javafx.geometry.Pos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
