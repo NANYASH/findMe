9e7ace95968334ab7764ea5602b46639bb82547d
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-03-10 10:43:34 +0200

    init

--- src/main/java/com/findMe/exception/InternalServerError.java
+++ src/main/java/com/findMe/exception/InternalServerError.java
@@ -1,6 +1,9 @@
 package com.findMe.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class InternalServerError extends Exception{
     public InternalServerError(String cause) {
         super(cause);
